b1ef0752c64de69fcc9e188f9ec068d60cf934cb
seung_hb <seung.hb@teamsparta.co>
2026-05-27 13:48:03 +0900

    fix: reactions_add 실패가 telegram.send 막지 않도록 분리

--- agents/seung_hb/handler.ts
+++ agents/seung_hb/handler.ts
@@ -2,15 +2,16 @@ import { defineHandler } from '@rego/runtime-sdk';
 
 export default defineHandler({
   async onSlackMention(event, ctx) {
-    await Promise.all([
-      ctx.tools['slack.reactions_add']!({
-        channel: event.channel,
-        ts: event.ts,
-        name: 'eyes',
-      }),
-      ctx.tools['telegram.send']!({
-        text: `슬랙 멘션이 왔어요!\n\nfrom: ${event.userName ?? event.user}\nch: #${event.channelName ?? event.channel}\n\n${event.text}`,
-      }),
-    ]);
+    await ctx.tools['telegram.send']!({
+      text: `슬랙 멘션이 왔어요!\n\nfrom: ${event.userName ?? event.user}\nch: #${event.channelName ?? event.channel}\n\n${event.text}`,
+    });
+
+    await ctx.tools['slack.reactions_add']!({
+      channel: event.channel,
+      ts: event.ts,
+      name: 'eyes',
+    }).catch((err: unknown) => {
+      ctx.logger.warn('reactions_add 실패', { error: String(err) });
+    });
   },
 });
